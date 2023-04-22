ff7e6b14499f4490421efd938cf44d414c05e060
Wruthless <wsgnzls@comcast.net>
2023-04-22 03:44:25 -0400

    Added a generic based stack example.

--- src/main/java/datastructures/Stack.java
+++ src/main/java/datastructures/Stack.java
@@ -3,35 +3,64 @@ package datastructures;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
+
 public class Stack<T> {
 
     private final ArrayList<T> elements;
 
-
-    public Stack() {
+    Stack() {
         this(10);
     }
 
-
-    public Stack(int capacity) {
-
-        int initCapacity = capacity > 0 ? capacity : 10;
-        elements = new ArrayList<T>(initCapacity);
-
+    Stack(int capacity) {
+        int initialCapacity = capacity > 0 ? capacity : 10;
+        elements = new ArrayList<>(initialCapacity);
     }
 
-    public void push(T pushValue) {
-        elements.add(pushValue);
+    public void push(T element) {
+        elements.add(element);
     }
 
     public T pop() {
 
-        if(elements.isEmpty()) {
+        if( elements.isEmpty() ) {
             throw new EmptyStackException();
         }
 
         return elements.remove(elements.size() - 1);
-
     }
-
 }
+
+
+//public class Stack<T> {
+//
+//    private final ArrayList<T> elements;
+//
+//
+//    public Stack() {
+//        this(10);
+//    }
+//
+//
+//    public Stack(int capacity) {
+//
+//        int initCapacity = capacity > 0 ? capacity : 10;
+//        elements = new ArrayList<T>(initCapacity);
+//
+//    }
+//
+//    public void push(T pushValue) {
+//        elements.add(pushValue);
+//    }
+//
+//    public T pop() {
+//
+//        if(elements.isEmpty()) {
+//            throw new EmptyStackException();
+//        }
+//
+//        return elements.remove(elements.size() - 1);
+//
+//    }
+//
+//}
--- src/main/java/datastructures/StackImpl.java
+++ src/main/java/datastructures/StackImpl.java
@@ -2,28 +2,28 @@ package datastructures;
 
 import java.util.EmptyStackException;
 
+
 public class StackImpl {
 
     public static void main(String[] args) {
 
-        Double[] doubleElements = { 1.1, 2.2, 3.3, 4.4, 5.5 };
-        Integer[] integerElements = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Double[] doubles = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+        Integer[] integers = { 1, 2, 3, 4, 5 };
 
-        Stack<Double> dStack = new Stack<>(5);
-        Stack<Integer> iStack = new Stack<>();
+        Stack<Double> doubleStack = new Stack<>(5);
+        Stack<Integer> integerStack = new Stack<>();
 
-        testPush(dStack, doubleElements);
-        testPop(dStack);
+        testPush(doubleStack, doubles);
+        testPop(doubleStack);
 
-        testPush(iStack, integerElements);
-        testPop(iStack);
+        testPush(integerStack, integers);
+        testPop(integerStack);
 
     }
 
-
     public static <T> void testPush(Stack<T> stack, T[] elements) {
 
-        System.out.printf("%s%s%n", "Pushed elements: ", elements.getClass());
+        System.out.printf("%s%s%n", "Elements: ", elements.getClass());
 
         for ( T element : elements ) {
             System.out.printf("%s ", element);
@@ -32,7 +32,6 @@ public class StackImpl {
 
     }
 
-
     public static <T> void testPop(Stack<T> stack) {
 
         System.out.printf("%n%s%n", "Popped elements: ");
@@ -40,17 +39,70 @@ public class StackImpl {
         try {
             T popValue;
 
-            while ( true ) {
+            while (true) {
                 popValue = stack.pop();
                 System.out.printf("%s ", popValue);
             }
-        } catch ( EmptyStackException emptyStackException ) {
+        } catch ( EmptyStackException e ) {
             System.out.println();
-            emptyStackException.printStackTrace();
+            e.printStackTrace();
         }
-        System.out.println();
 
+        System.out.println();
     }
 
-
 }
+
+
+//public class StackImpl {
+//
+//    public static void main(String[] args) {
+//
+//        Double[] doubleElements = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+//        Integer[] integerElements = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//
+//        Stack<Double> dStack = new Stack<>(5);
+//        Stack<Integer> iStack = new Stack<>();
+//
+//        testPush(dStack, doubleElements);
+//        testPop(dStack);
+//
+//        testPush(iStack, integerElements);
+//        testPop(iStack);
+//
+//    }
+//
+//
+//    public static <T> void testPush(Stack<T> stack, T[] elements) {
+//
+//        System.out.printf("%s%s%n", "Pushed elements: ", elements.getClass());
+//
+//        for ( T element : elements ) {
+//            System.out.printf("%s ", element);
+//            stack.push(element);
+//        }
+//
+//    }
+//
+//
+//    public static <T> void testPop(Stack<T> stack) {
+//
+//        System.out.printf("%n%s%n", "Popped elements: ");
+//
+//        try {
+//            T popValue;
+//
+//            while ( true ) {
+//                popValue = stack.pop();
+//                System.out.printf("%s ", popValue);
+//            }
+//        } catch ( EmptyStackException emptyStackException ) {
+//            System.out.println();
+//            emptyStackException.printStackTrace();
+//        }
+//        System.out.println();
+//
+//    }
+//
+//
+//}
